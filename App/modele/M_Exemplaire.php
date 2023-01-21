fe68bc634c4e2867cad3b61e44e67fdc0bdd6818
Olena ILCHENKO <olena.ilchenko@etud.gretacfa-montpellier.fr>
2023-01-21 09:23:56 +0100

    ajout PHP_TP-lord_of_geek

---
+++ App/modele/M_Exemplaire.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Requetes sur les exemplaires  de jeux videos
+ *
+ * @author Loic LOG
+ */
+class M_Exemplaire {
+
+    /**
+     * Retourne sous forme d'un tableau associatif tous les jeux de la
+     * catégorie passée en argument
+     *
+     * @param $idCategorie
+     * @return un tableau associatif
+     */
+    public static function trouveLesJeuxDeCategorie($idCategorie) {
+        $req = "SELECT * FROM exemplaires WHERE categorie_id = '$idCategorie'";
+        $res = AccesDonnees::query($req);
+        $lesLignes = $res->fetchAll();
+        return $lesLignes;
+    }
+
+    /**
+     * Retourne les jeux concernés par le tableau des idProduits passée en argument
+     *
+     * @param $desIdJeux tableau d'idProduits
+     * @return un tableau associatif
+     */
+    public static function trouveLesJeuxDuTableau($desIdJeux) {
+        $nbProduits = count($desIdJeux);
+        $lesProduits = array();
+        if ($nbProduits != 0) {
+            foreach ($desIdJeux as $unIdProduit) {
+                $req = "SELECT * FROM exemplaires WHERE id = '$unIdProduit'";
+                $res = AccesDonnees::query($req);
+                $unProduit = $res->fetch();
+                $lesProduits[] = $unProduit;
+            }
+        }
+        return $lesProduits;
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# lord_of_geek
+
+Prototype qui propose une façon d'organiser son code pour débuter...
+
+Les modifications suivantes doivent être apportées:
+1) Ajoutez une table contenant des informations sur les clients à la base de données.
+2) Créez une relation entre les commandes et les clients (un client peut former plusieurs commandes).
+3) Créez un formulaire d'enregistrement des clients sur le site web.
+4) Créez un formulaire d'authentification sur le site web. (Connexion)
+5) Faites une démonstration de tous les jeux disponibles à la vente.
+6) Lorsqu'un client passe une commande pour un jeu, celui-ci doit être exclu de la base de données (transféré au statut commandé).
+7) Ajouter des jeux au panier, mais lorsque l'utilisateur confirme sa commande, vous devez afficher le formulaire d'authentification .
+8) Permettre de modifier les informations relatives aux clients dans leur compte
+9) Afficher la liste des commandes de l’utilisateur
+10) Ajouter une page mon compte avec les informations de l’utilisateur
